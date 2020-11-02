e14d9006dd7cb86375f012b20402165e5cc27c3f
Simon Wakenhut <simon.wakenhut@telekom.de>
2020-11-02 13:10:27 +0100

    Fix user management

--- src/components/UserManagement.vue
+++ src/components/UserManagement.vue
@@ -1,10 +1,11 @@
 <template>
   <v-container class="mt-16 user-management">
 
-    <v-snackbar v-model="showPasswordSnackbar" timeout="15000">
-      The initial user password is "{{ initialPassword }}". The user can also use the "Forgot password" function the set their password.
+    <!-- Password Snackbar -->
+    <v-snackbar v-model="snackbarPassword" timeout="15000">
+      The initial user password is "{{ snackbarPasswordInitialPassword }}". The user can also use the "Forgot password" function the set their password.
       <template v-slot:action="{ attrs }">
-        <v-btn color="white" text v-bind="attrs" @click="showPasswordSnackbar = false" >
+        <v-btn color="white" text v-bind="attrs" @click="snackbarPassword = false" >
           <v-icon>mdi-close</v-icon>
         </v-btn>
       </template>
@@ -17,9 +18,9 @@
           <v-spacer></v-spacer>
 
           <!-- New User Dialog -->
-          <v-dialog v-model="dialog" max-width="500px">
+          <v-dialog v-model="dialogCreateUser" max-width="500px">
             <template v-slot:activator="{ on, attrs }">
-              <v-btn v-bind="attrs" v-on="on" icon @click="createUserMessageEnabled = false">
+              <v-btn v-bind="attrs" v-on="on" icon @click="spawnUserCreationDialog">
                 <v-icon>mdi-account-plus</v-icon>
               </v-btn>
             </template>
@@ -32,26 +33,26 @@
               <v-container>
                 <v-row>
                   <v-col cols="12" sm="6" md="12">
-                    <v-text-field v-model="newUser.uid" label="UID" placeholder="firstname.lastname"></v-text-field>
+                    <v-text-field v-model="dialogCreateUserNewUser.uid" label="UID" placeholder="firstname.lastname"></v-text-field>
                   </v-col>
                   <v-col cols="12" sm="6" md="6">
-                    <v-text-field v-model="newUser.firstname" label="First Name" placeholder="Max"></v-text-field>
+                    <v-text-field v-model="dialogCreateUserNewUser.firstname" label="First Name" placeholder="Max"></v-text-field>
                   </v-col>
                   <v-col cols="12" sm="6" md="6">
-                    <v-text-field v-model="newUser.lastname" label="Last Name" placeholder="Mustermann"></v-text-field>
+                    <v-text-field v-model="dialogCreateUserNewUser.lastname" label="Last Name" placeholder="Mustermann"></v-text-field>
                   </v-col>
                   <v-col cols="12" sm="6" md="12">
-                    <v-text-field v-model="newUser.mail" label="E-Mail" placeholder="max.mustermann@example.com"></v-text-field>
+                    <v-text-field v-model="dialogCreateUserNewUser.mail" label="E-Mail" placeholder="max.mustermann@example.com"></v-text-field>
                   </v-col>
                 </v-row>
-                <v-alert type="error" v-show="createUserMessageEnabled">{{ createUserMessage }}</v-alert>
+                <v-alert type="error" v-show="dialogCreateUserErrorAlert">{{ dialogCreateUserErrorMessage }}</v-alert>
               </v-container>
             </v-card-text>
 
               <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="closeCreate">Cancel</v-btn>
-                <v-btn color="blue darken-1" text @click="createUser">Create</v-btn>
+                <v-btn color="blue darken-1" text @click="destroyUserCreationDialog">Cancel</v-btn>
+                <v-btn color="blue darken-1" text @click="createUser(dialogCreateUserNewUser)">Create</v-btn>
               </v-card-actions>
             </v-card>
           </v-dialog>
@@ -61,13 +62,13 @@
           </v-btn>
           
           <!-- Delete User Dialog -->
-          <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-dialog v-model="dialogDeleteUser" max-width="500px">
             <v-card>
               <v-card-title class="headline">Are you sure you want to delete this user?</v-card-title>
               <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-                <v-btn color="blue darken-1" text @click="deleteUser">Delete</v-btn>
+                <v-btn color="blue darken-1" text @click="destroyUserDeletionDialog">Cancel</v-btn>
+                <v-btn color="blue darken-1" text @click="deleteUser(dialogDeleteUserSelected)">Delete</v-btn>
                 <v-spacer></v-spacer>
               </v-card-actions>
             </v-card>
@@ -76,7 +77,7 @@
       </template>
 
       <template v-slot:item.actions="{ item }">
-        <v-btn icon :disabled="disableDelete(item)" @click="deleteUserDialog(item)">
+        <v-btn icon :disabled="disableDelete(item)" @click="spawnUserDeletionDialog(item)">
           <v-icon>mdi-delete</v-icon>
         </v-btn>
       </template>
@@ -88,8 +89,31 @@
 <script>
   export default {
     data: () => ({
-      dialog: false,
-      dialogDelete: false,
+      // Dialog
+      dialogCreateUser: false,
+      dialogCreateUserErrorAlert: false,
+      dialogCreateUserErrorMessage: '',
+      dialogCreateUserNewUser: {
+        uid: '',
+        firstname: '',
+        lastname: '',
+        mail: '',
+        password: ''
+      },
+      dialogCreateUserDefaultUser: {
+        uid: '',
+        firstname: '',
+        lastname: '',
+        mail: '',
+        password: ''
+      },
+      dialogDeleteUser: false,
+      dialogDeleteUserSelected: {},
+      // Snackbar
+      snackbarPassword: false,
+      snackbarPasswordInitialPassword: '',
+      // Table
+      users: [],
       headers: [
         {
           text: 'UID',
@@ -117,89 +141,68 @@
         },
         { text: 'Actions', value: 'actions', sortable: false },
       ],
-      showPasswordSnackbar: false,
-      initialPassword: '',
-      deleteUserObject: {},
-      createUserMessage: '',
-      createUserMessageEnabled: false,
-      users: [],
-      newUser: {
-        uid: '',
-        firstname: '',
-        lastname: '',
-        mail: '',
-        password: ''
-      },
-      defaultUser: {
-        uid: '',
-        firstname: '',
-        lastname: '',
-        mail: '',
-        password: ''
-      },
     }),
-    computed: {
-
-    },
     created () {
       this.getUser()
     },
     methods: {
-      disableDelete(item) {
-        return this.$keycloak.userName == item.uid || this.users.length == 1
+      // Dialogs
+      spawnUserCreationDialog() {
+        this.dialogCreateUser = true
       },
+      spawnUserDeletionDialog(user) {
+        this.dialogDeleteUser = true
+        this.dialogDeleteUserSelected = user
+      },
+      destroyUserCreationDialog() {
+        this.dialogCreateUser = false       // Disable dialog
+        this.dialogCreateUserNewUser = Object.assign({}, this.dialogCreateUserDefaultUser)  // Clear inputs in dialog
+        this.dialogCreateUserErrorMessage = ''    // Clear alert in dialog
+        this.dialogCreateUserErrorAlert = false   // Clear alert in dialog
+        this.getUser()                      // Update table
+      },
+      destroyUserDeletionDialog() {
+        this.dialogDeleteUser = false       // Disable dialog
+        this.dialogDeleteUserSelected = {}  // Empty selected user
+        this.getUser()                      // Update table
+      },
+      // Users Tasks
       getUser()
       {
         this.axios.get('https://' + this.$config.domains.api + '/users').then((response) => {
           this.users = response.data
         })
       },
-      deleteUser() {
-        this.axios.delete('https://' + this.$config.domains.api + '/user/' + this.deleteUserObject.uid).then((response) => {
-          if(response.data.status == "successfull") {
+      deleteUser(user) {
+        this.axios.delete('https://' + this.$config.domains.api + '/user/' + user.uid).then((response) => {
+          if(response.data.status == "successful") {
             
-            this.closeDelete()
+            this.destroyUserDeletionDialog()
           } else {
             console.error(response.data)
           }
         })
       },
-      deleteUserDialog(item) {
-        this.deleteUserObject = item
-        this.dialogDelete = true
-      },
-      closeCreate() {
-        this.dialog = false
-        this.$nextTick(() => {
-          this.newUser = Object.assign({}, this.defaultUser)
-        })
-        this.getUser()
-        this.createUserMessageEnabled = false
-      },
-      closeDelete() {
-        this.deleteUserObject = {}
-        this.dialogDelete = false
-        this.$nextTick(() => {
-          this.newUser = Object.assign({}, this.newUser)
-        })
-        this.getUser()
-      },
-      createUser() {
-        this.newUser.password = Math.random().toString(36).substring(2)
+      createUser(user) {
+        user.password = Math.random().toString(36).substring(2)
 
-        this.axios.put('https://' + this.$config.domains.api + '/user', this.newUser).then((response) => {
-          if(response.data.status == "successfull") {
-            this.initialPassword = this.newUser.password
-            this.showPasswordSnackbar = true
+        this.axios.put('https://' + this.$config.domains.api + '/user', user).then((response) => {
+          if(response.data.status == "successful") {
+            this.snackbarPasswordInitialPassword = user.password
+            this.snackbarPassword = true
 
-            this.closeCreate()
+            this.destroyUserCreationDialog()
           } else if(response.data.status == "failed") {
-            this.createUserMessageEnabled = true
-            this.createUserMessage = response.data.status.message
+            this.dialogCreateUserErrorAlert = true
+            this.dialogCreateUserErrorMessage = response.data.status.message
             console.error(response.data)
           }
         })
       },
+      // Computed
+      disableDelete(item) {
+        return this.$keycloak.userName == item.uid || this.users.length == 1
+      }
     },
   }
 </script>
